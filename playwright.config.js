d2e65d55e2f6a5c0ff965833240ece7178be233f
harshitksrivastava <harsh0311@gmail.com>
2026-02-15 03:10:46 +0530

    updated headless propertyas headless:false fails on CI

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     // either install Xserver or set headless to false to see the browser actions
-    headless: false,
+    // headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
